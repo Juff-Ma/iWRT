7ef7e12dec762e5a19d142a021f0b39f94b74ffc
Juff-Ma <contact@juffma.de>
2026-03-08 11:36:04 +0100

    Remove workflows
    Signed-off-by: Juff-Ma <contact@juffma.de>

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
-echo "Cleaning up PATH"
+echo "Cleaning up PATH and adding TTY"
+export GPG_TTY=$(tty)
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 echo "Done! If this didn't work, remember this script needs to be sourced"
