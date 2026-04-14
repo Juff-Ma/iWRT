9772fc42a0704019ff2c0c01d4770d819125eec0
Julian Rossbach <contact@juffma.de>
2026-04-14 16:02:05 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,3 @@
-echo "Cleaning up PATH and adding TTY"
-export GPG_TTY=$(tty)
+echo "Cleaning up PATH"
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 echo "Done! If this didn't work, remember this script needs to be sourced"
